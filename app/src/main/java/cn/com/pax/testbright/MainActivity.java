908a7c65d4ac6bc52588dec901df12950a387d52
lgsd123456 <hbhg8612814@gmail.com>
2019-08-27 23:36:40 +0800

    设置亮度

--- app/src/main/java/cn/com/pax/testbright/MainActivity.java
+++ app/src/main/java/cn/com/pax/testbright/MainActivity.java
@@ -1,7 +1,16 @@
 package cn.com.pax.testbright;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText =  findViewById(R.id.brightvalue);
+        Button button = findViewById(R.id.setbright);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String brightStr = editText.getText().toString();
+                if(brightStr.trim().length() > 0) {
+                    setBrightness(MainActivity.this, Integer.parseInt(brightStr));
+                }
+            }
+        });
     }
+
+    public static void setBrightness(Activity activity, int brightness) {
+        // Settings.System.putInt(activity.getContentResolver(),
+
+        // Settings.System.SCREEN_BRIGHTNESS_MODE,
+
+        // Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+
+        WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+
+        lp.screenBrightness = Float.valueOf(brightness) * (1f / 255f);
+        Log.d("lxy", "set  lp.screenBrightness == " + lp.screenBrightness);
+
+        activity.getWindow().setAttributes(lp);
+    }
+
+//    public static boolean isAutoBrightness(ContentResolver aContentResolver) {
+//        boolean automicBrightness = false;
+//        try {
+//            automicBrightness = Settings.System.getInt(aContentResolver,
+//                    Settings.System.SCREEN_BRIGHTNESS_MODE) == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
+//        } catch (Settings.SettingNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        return automicBrightness;
+//    }
+//
+//    public static void stopAutoBrightness(Activity activity) {
+//
+//        Settings.System.putInt(activity.getContentResolver(),
+//
+//                Settings.System.SCREEN_BRIGHTNESS_MODE,
+//
+//                Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+//    }
+
 }
